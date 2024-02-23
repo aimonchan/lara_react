aa2a5b8a7a2c794ca5dfec6314ccae28135505a2
aimonoutlook <150989322+aimonoutlook@users.noreply.github.com>
2024-02-23 09:16:07 +0000

    useRef in Signup

--- blog/src/views/Signup.jsx
+++ blog/src/views/Signup.jsx
@@ -1,18 +1,31 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 export default function Signup(){
+
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
     
     const onSubmit=(ev)=>{
         ev.preventDefault();
+        const payload = {
+            name:nameRef.current.value,
+            email:emailRef.current.value,
+            password:passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        console.log(payload);
     }
 
     return(
     
         <form class="form-inline" onSubmit={onSubmit}>
             <h4 className="text-center">Sign Up For Free!</h4>
-            <input placeholder="Full Name" type="text" class="form-control mt-3 mb-1" id="email"></input>
-            <input placeholder="Email Address" type="email" class="form-control mt-3 mb-1" id="email"></input>
-            <input placeholder="Password" type="password" class="form-control mt-3 mb-1"></input>      
-            <input placeholder="Confirm your Password" type="password" class="form-control mt-3 mb-3"></input>
+            <input ref={nameRef} placeholder="Full Name" class="form-control mt-3 mb-1"/>
+            <input ref={emailRef} placeholder="Email Address"  class="form-control mt-3 mb-1" />
+            <input ref={passwordRef} placeholder="Password" type="password" class="form-control mt-3 mb-1"></input>      
+            <input ref={passwordConfirmationRef}placeholder="Confirm your Password" type="password" class="form-control mt-3 mb-3"></input>
     
                 
             <Link className="text-center nav-link mb-3 text-primary" to="/login">Already have an account!</Link>
